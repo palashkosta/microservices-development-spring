37f8a043d562b5c154260fd3d5f8a28f5372d027
Palash <mobiledev.palash@gmail.com>
2021-03-10 15:31:31 +0530

    Added currency-exchange-service microservices

---
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,30 @@
+package com.learning.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+    
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExcahnge retrieveExcahngeValue(
+        @PathVariable String from, 
+        @PathVariable String to) {
+
+            CurrencyExcahnge currencyExcahnge 
+                = new CurrencyExcahnge(1000L, from, to, BigDecimal.valueOf(50));
+            String port = environment.getProperty("local.server.port");
+            currencyExcahnge.setEnvironment(port);
+            return currencyExcahnge;
+
+    }
+
+}

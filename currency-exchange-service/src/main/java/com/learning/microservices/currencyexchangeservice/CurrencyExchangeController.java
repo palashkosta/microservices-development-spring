2b0db2a5d17a3fd73a2795377965e68e67ac111f
Palash <mobiledev.palash@gmail.com>
2021-03-15 20:07:10 +0530

    Final changes before docker container

--- currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package com.learning.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
@@ -21,6 +23,8 @@ public class CurrencyExchangeController {
     public CurrencyExcahnge retrieveExcahngeValue(
         @PathVariable String from, 
         @PathVariable String to) {
+
+            logger.info("retrieve exchange value - called with {} to {}", from , to);
 
             // CurrencyExcahnge currencyExcahnge = new CurrencyExcahnge(1000L, from, to, BigDecimal.valueOf(50));
             CurrencyExcahnge currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
